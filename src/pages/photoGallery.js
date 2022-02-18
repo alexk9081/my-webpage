75658dd80e8cba17e667ff9f26f2a53b27c6823f
alexk9081 <alexk9081@gmail.com>
2022-02-18 15:37:59 -0500

    Got firebase listAll working

--- src/pages/photoGallery.js
+++ src/pages/photoGallery.js
@@ -4,7 +4,7 @@ import classes from './photoGallery.module.css';
 import { useState, useEffect } from 'react';
 
 import { initializeApp } from "firebase/app";
-import { getStorage, ref, getDownloadURL, list } from "firebase/storage";
+import { getStorage, ref, getDownloadURL, listAll } from "firebase/storage";
 import Photo from "../components/photo";
 
 
@@ -17,28 +17,49 @@ function GalleryPage() {
         setIsLoading(true);
 
         const firebaseConfig = {
-          apiKey: "AIzaSyAqJg-fe3fayu8xapocNCJSzKm4XGhzq5E",
-          authDomain: "personalwebsite-51254.firebaseapp.com",
-          projectId: "personalwebsite-51254",
-          storageBucket: "personalwebsite-51254.appspot.com",
-          messagingSenderId: "32709056754",
-          appId: "1:32709056754:web:3a1cefca604f4be665586b"
+            apiKey: "AIzaSyAqJg-fe3fayu8xapocNCJSzKm4XGhzq5E",
+            authDomain: "personalwebsite-51254.firebaseapp.com",
+            projectId: "personalwebsite-51254",
+            storageBucket: "personalwebsite-51254.appspot.com",
+            messagingSenderId: "32709056754",
+            appId: "1:32709056754:web:3a1cefca604f4be665586b"
         };
         const app = initializeApp(firebaseConfig);
         const storage = getStorage(app);
 
-        // const listRef = ref(storage, "anm");
-        // const firstPage = list(listRef, { maxResults: 100 });
-        
-        // setReturnedPictures(firstPage.items);
+        // var storageRef = firebase.storage().ref("anm");
+        var storageRef = ref(storage, 'anm');
+
+        listAll(storageRef).then(function (result) {
+            console.log("getting files");
+
+            result.items.forEach(function (imageRef) {
+                console.log("Getting image");
+                displayImage(imageRef);
+            });
+
+            console.log("done");
+        }).catch(function (error) {
+            console.log(error);
+            console.log("Unable to get storage");
+        });
+
+        function displayImage(imageRef) {
+            getDownloadURL(ref(storage, imageRef)).then(function (url) {
+                console.log(url);
+            }).catch(function (error) {
+                console.log(error);
+                console.log("Unable to display image");   
+            });
+        }
 
         getDownloadURL(ref(storage, 'anm/1E9FFE32-FD68-4B4D-93AF-E0B2890B5FFD.jpeg'))
-        .then((url) => {
-            setPicture(url);
-            setIsLoading(false);
-        })
-        .catch((error) => {
-        });
+            .then((url) => {
+                setPicture(url);
+                setIsLoading(false);
+            })
+            .catch((error) => {
+            });
 
     }, []);
 
@@ -50,7 +71,7 @@ function GalleryPage() {
 
     return <div className={classes.photosBox}>
         <div>
-            <Photo src={picture}/>
+            <Photo src={picture} />
         </div>
         <PhotoColumn />
         <PhotoColumn />
