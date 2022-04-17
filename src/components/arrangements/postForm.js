0a81116a249ca17d8aef8f9d369991e49e526143
alexk9081 <alexk9081@gmail.com>
2022-04-17 11:48:31 -0400

    Reformatted blog posts

--- src/components/arrangements/postForm.js
+++ src/components/arrangements/postForm.js
@@ -2,13 +2,25 @@ import Input from '../inputBox'
 import classes from './postForm.module.css';
 
 function PostForm() {
+    function getInputsFromForm(){
+        console.log("getInputsFromForm");
+        const inputs = document.querySelectorAll(`.post`);
+        const inputsArray = [];
+        inputs.forEach(input => {
+            console.log("added item to array");
+            inputsArray.push(input.value);
+            console.log(input.value);
+        });
+        return inputsArray;
+    }
+
     return <div className={classes.form}>
         <form className={classes.post}>
-            <Input id="title" label="Title" placeHolder="My First Post" />
-            <Input id="description" label="Description" placeHolder="I did this today." />
-            <Input id="user" label="Username" placeHolder="KeoA" />
+            <Input className="input" id="title" label="Title" placeHolder="My First Post" />
+            <Input className="input" id="description" label="Description" placeHolder="I did this today." />
+            <Input className="input" id="user" label="Username" placeHolder="KeoA" />
         </form>
-        <button className={classes.submit}>Submit</button>
+        <button className={classes.submit} onClick={getInputsFromForm}>Submit</button>
     </div>
 }
 
--- src/components/blogPost.js
+++ src/components/blogPost.js
@@ -1,28 +1,39 @@
 import classes from './blogPost.module.css';
 
 function BlogPost(props) {
+
+    function getCurrentTimeAndDate() {
+        const date = new Date();
+        const day = date.getDate();
+        const month = date.getMonth() + 1;
+        const year = date.getFullYear();
+        const hours = date.getHours();
+        const minutes = date.getMinutes();
+
+        const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        const monthName = monthNames[month - 1];
+
+        return `${monthName} ${day}, ${year} ${hours}:${minutes}`;
+    }
+
     return <div className={classes.post}>
-        <div className={classes.sect}>
-            <h2 className={classes.title}>
-                Lorem Ipsum Dolor
-            </h2>
-            <h3 className={classes.date}>
-                Date: 02/21/2021 @ 21:47
-            </h3>
-        </div>
-        <div className={classes.sect}>
-            <p className={classes.desc}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-        </div>
-        <div className={classes.sect}>
-            <h4>
-                Signature
-            </h4>
-            <p>
-                Whats poppin'
-            </p>
-        </div>
+        <h2 className={classes.title}>
+            Lorem Ipsum Dolor
+        </h2>
+        <h3 className={classes.date}>
+            {getCurrentTimeAndDate()}
+        </h3>
+        <hr className={classes.sectionBreak}/>
+        <p className={classes.desc}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <hr className={classes.sectionBreak}/>
+        <h4 className={classes.signatureTitle}>
+            Signature
+        </h4>
+        <p className={classes.signatureBody}>
+            Whats poppin'
+        </p>
     </div>
 }
 
