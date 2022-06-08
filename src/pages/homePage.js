362256b784982c9c6ddad40348ba5b2128833131
alexk9081 <alexk9081@gmail.com>
2022-06-08 19:49:36 -0400

    Removed scrollbar

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,19 +1,30 @@
 import IntroBox from "../components/introBox";
 import classes from "./homePage.module.css"
 import BlogPost from "../components/blogPost.js";
+import { useEffect, useRef } from "react";
 
 function HomePage() {
+    const hidingContent = useRef(null);
+    const scrollingContent = useRef(null);
+
+    useEffect(() => {
+        hidingContent.current.style.width = scrollingContent.current.clientWidth + "px";
+    }, []);
+
+
     return <div className={classes.home}>
-        <main className={classes.mainContent}>
-            <IntroBox />
-            <section>
-                <BlogPost />
-                <BlogPost />
-                <BlogPost />
-                <BlogPost />
-                <BlogPost />
-            </section>
-        </main>
+        <div className={classes.hiderDiv} ref={hidingContent} >
+            <main className={classes.mainContent} ref={scrollingContent}>
+                <IntroBox />
+                <section>
+                    <BlogPost />
+                    <BlogPost />
+                    <BlogPost />
+                    <BlogPost />
+                    <BlogPost />
+                </section>
+            </main>
+        </div>
     </div>
 }
 
