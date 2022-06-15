7ef79a024dd8cec17183699bd732dd30576803a0
alexk9081 <alexk9081@gmail.com>
2022-06-15 19:42:55 -0400

    Edited formatting

--- src/pages/aboutMePage.js
+++ src/pages/aboutMePage.js
@@ -4,14 +4,20 @@ function ProjectInfoPage() {
     return <div className={classes.profileInfo}>
         <img src="https://avatars.githubusercontent.com/u/89793763?v=4" alt="Author" className={classes.profileImg} width="200px" />
         <br/>
-        I'm Alex, and I am a Computer Science major at the University of North Florida. Currently, I am the most proficient in Java but I have experience working on projects using Python, C, and JavaScript.
+        <p className={classes.profileText}>
+            I'm Alex, and I am a Computer Science major at the University of North Florida. Currently, I am the most proficient in Java but I have experience working on projects using Python, C, and JavaScript.
+        </p>
         <br/>
-        My current focus is working on expanding my skillset and learning best practices.
-        <br/>
-        <br/>
-        Github: alexk9081
-        <br/>
-        Email: alexk9081@gmail
+        <p className={classes.profileText}>
+            My current focus is working on expanding my skillset and learning best practices.
+        </p>
+        <br/><br/>
+        <p className={classes.profileLinks}>
+            <b>Github:</b> <a className={classes.profileHyperLink} href="https://github.com/alexk9081">AlexK9081</a>
+        </p>
+        <p className={classes.profileLinks}>
+            <b>Email:</b> <a className={classes.profileHyperLink} href="mailto:alexk9081@gmail.com">AlexK9081@gmail.com</a>
+        </p>
         <br/>
     </div>
 }
--- src/pages/testPage.js
+++ src/pages/testPage.js
@@ -1,9 +1,21 @@
 import TestDiv from "../components/testDIv";
 import PostForm from '../components/arrangements/blogPostForm.js';
+import { useState } from 'react';
 
 function TestPage() {
+    const [show, setShow] = useState(false);
+
+    function flip() {
+        setShow(!show);
+    }
+
     return <div>
+
         Test Page
+
+        {show && <p>show is true</p>}
+
+        <button onClick={flip}>flip</button>
         <TestDiv></TestDiv>
 
         <PostForm />
