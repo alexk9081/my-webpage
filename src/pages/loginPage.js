cce80c2bd2b8ddf681f20feff1c7dfb851b23add
alexk9081 <alexk9081@gmail.com>
2022-06-15 19:43:25 -0400

    Fixed getting auth information

--- src/pages/loginPage.js
+++ src/pages/loginPage.js
@@ -1,6 +1,6 @@
 import classes from "./loginPage.module.css"
 import { auth, provider } from "../firebaseConfig"
-import { signInWithRedirect, getRedirectResult, signOut, GoogleAuthProvider } from "firebase/auth"
+import { signInWithRedirect, signOut } from "firebase/auth"
 
 function Login() {
     let googleLogin = () => {
@@ -18,39 +18,12 @@ function Login() {
     }
 
     let getResults = () => {
-        getRedirectResult(auth)
-        .then((result) => {
-            if(result == null){
-                console.log("No user logged in");
-                return;
-            }
-            // This gives you a Google Access Token. You can use it to access Google APIs.
-            const credential = GoogleAuthProvider.credentialFromResult(result);
-            const token = credential.accessToken;
-
-            // The signed-in user info.
-            const user = result.user;
-            console.log(user.displayName + " signed in using " + user.email);
-            console.log("Verified: " + user.emailVerified);
-            console.log("UID: " + user.uid);
-            console.log("Metadata: Creation-" + user.metadata.creationTime + " LastSignin-" + user.metadata.lastSignInTime);
-            console.log("User token: " + token);
-        }).catch((error) => {
-            // Handle Errors here.
-            const errorCode = error.code;
-            const errorMessage = error.message;
-
-            // The email of the user's account used.
-            const email = error.customData.email;
-            // The AuthCredential type that was used.
-            const credential = GoogleAuthProvider.credentialFromError(error);
-
-            console.log("Error signing in " + email + "::" + credential);
-            console.log(errorCode + ": " + errorMessage);
-            console.log(error);
-        });
-    }
-
+        if(auth.currentUser == null){
+            console.log("No user logged in");
+            return;
+        }
+        console.log(auth.currentUser.email);
+    };
 
 
     return <div>
