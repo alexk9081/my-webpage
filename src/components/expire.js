a05ac6356641caafcb8f86c5831e2a31164c51fc
alexk9081 <alexk9081@gmail.com>
2022-06-21 22:42:35 -0400

    Added protected route message

---
+++ src/components/expire.js
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from "react";
+import classes from "./expire.module.css";
+
+function Expire(props) {
+    const [visible, setVisible] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setVisible(false);
+        }, props.delay);
+        return () => clearTimeout(timer)
+    }, [props.delay]);
+
+    return visible ? <div className={classes.errorMessage}>{props.children}</div> : <div className={classes.errorMessage} />;
+}
+
+export default Expire;
--- src/components/protectedRoute.js
+++ src/components/protectedRoute.js
@@ -1,18 +1,22 @@
 import { Route, Redirect } from "react-router-dom";
 import { auth } from "../firebaseConfig";
+import Expire from "./expire";
+
 
 function ProtectedRoute({ component: Component, ...rest }) {
+    let routingCondition = (props) => {
+        if (auth.currentUser != null) {
+            return <Component {...props} />
+        }
+        else {
+            console.log("Unable to redirect");
+            <Expire delay={10}>Error</Expire>
+            return <Redirect to='/login' />
+        }
+    };
+
     return (
-        <Route
-            {...rest}
-            render={(props) =>
-                auth.currentUser != null ? (
-                    <Component {...props} />
-                ) : (
-                    <Redirect to='/login' />
-                )
-            }
-        />
+        <Route render= {routingCondition} {...rest} />
     );
 };
 
