3e87f5f2b8fcc969ece429d6c26b080b482b4921
alexk9081 <alexk9081@gmail.com>
2022-06-23 21:52:45 -0400

    Added conditional rendering to login page

--- src/pages/loginPage.js
+++ src/pages/loginPage.js
@@ -1,11 +1,15 @@
 import classes from "./LoginPage.module.css";
 import { auth, provider } from "../firebaseConfig";
 import { signInWithRedirect, signOut } from "firebase/auth";
+import { useState, useEffect } from "react";
 
 function Login() {
+    const [loggedIn, setLoggedIn] = useState(false);
+
     let googleLogin = () => {
         console.log("clicked");
         signInWithRedirect(auth, provider);
+        console.log("test");
     };
 
     let googleLogout = () => {
@@ -18,23 +22,41 @@ function Login() {
     }
 
     let getResults = () => {
-        if(auth.currentUser == null){
+        if (auth.currentUser == null) {
             console.log("No user logged in");
             return;
         }
         console.log(auth.currentUser.email);
     };
 
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged(user => {
+            if (user) {
+                setLoggedIn(true);
+            } else {
+                setLoggedIn(false);
+            }
+        });
+
+        // Cleanup subscription on unmount
+        return () => unsubscribe();
+    }, []);
 
     return <div>
-        <h1 className={classes.general}>Google sign-in</h1>
-        <button onClick={googleLogin}>Login with Google</button>
+        {loggedIn ?
+            <>
+                <h1 className={classes.general}>Google sign-out</h1>
+                <button onClick={googleLogout}>Logout with Google</button>
 
-        <h1 className={classes.general}>Google sign-out</h1>
-        <button onClick={googleLogout}>Logout with Google</button>
-
-        <h1 className={classes.general}>Results</h1>
-        <button onClick={getResults}>Get result</button>
+                <h1 className={classes.general}>Results</h1>
+                <button onClick={getResults}>Get result</button>
+            </>
+            :
+            <>
+                <h1 className={classes.general}>Google sign-in</h1>
+                <button onClick={googleLogin}>Login with Google</button>
+            </>
+        }
     </div>
 }
 
