66510b3a1be6a0f74e8b64c9e5447a6e7a211947
alexk9081 <alexk9081@gmail.com>
2022-07-05 21:12:15 -0400

    Unified navigation links

--- src/layout/NavigationBar.js
+++ src/layout/NavigationBar.js
@@ -9,6 +9,47 @@ import LoginButton from "../components/LoginButton";
 function NavigationBar(props) {
     const childRef = useRef();
 
+    const navItems = (isMobile) => {
+        let linkProps;
+        let loginButtonProps;
+
+        if (isMobile) {
+            linkProps = {
+                className: classes.navItem,
+                onClick: () => childRef.current.closeMenu()
+            }
+
+            loginButtonProps = {
+                className: [classes.navItem, classes.navLoginButton].join(' ')
+            }
+        }
+        else {
+            linkProps = {
+                className: classes.menuItem
+            }
+
+            loginButtonProps = {
+                className: [classes.menuItem, classes.menuLoginButton].join(' ')
+            }
+        }
+
+        return (
+            <>
+                {/* <Link {...linkProps} to='/test'>Test Page</Link> */}
+                {props.loginState &&
+                    <>
+                        <Link {...linkProps} to='/photo'>Add Photo</Link>
+                        <Link {...linkProps} to='/data'>Add Information</Link>
+                    </>
+                }
+                <Link {...linkProps} to='/gallery'>Image Gallery</Link>
+                <Link {...linkProps} to='/colors'>Color References</Link>
+                <Link {...linkProps} to='/info'>About Me</Link>
+                <LoginButton {...loginButtonProps} loginState={props.loginState} setLoginState={props.setLoginState} setUserState={props.setUserState} />
+            </>
+        )
+    }
+
     return <nav className={classes.navBar}>
         {/* Left home button */}
         <div>
@@ -20,39 +61,18 @@ function NavigationBar(props) {
 
         {/* Middle icon */}
         <FaRocket className={classes.logo} />
-        
+
         {/* Right desktop nav buttons */}
         <div className={classes.navList}>
             <DropdownMenu>
-                {/* <Link className={classes.menuItem} to='/test'>Test Page</Link> */}
-                {props.loginState && 
-                <>
-                    <Link className={classes.menuItem} to='/photo'>Add Photo</Link>
-                    <Link className={classes.menuItem} to='/data'>Add Information</Link>
-                </>
-                }
-                <Link className={classes.menuItem} to='/gallery'>Image Gallery</Link>
-                <Link className={classes.menuItem} to='/colors'>Color References</Link>
-                <Link className={classes.menuItem} to='/info'>About Me</Link>
-                <LoginButton className={[classes.menuItem, classes.menuLoginButton].join(' ')} loginState={props.loginState} setLoginState={props.setLoginState} setUserState={props.setUserState} />
+                {navItems(false)}
             </DropdownMenu>
         </div>
-        
+
         {/* Hamburger menu for mobile */}
         <FaBars className={classes.mobileMenuButton} onClick={() => childRef.current.openMenu()} />
         <MobileNavMenu ref={childRef}>
-            {props.loginState && 
-                <>
-                    <Link className={classes.navItem} onClick={() => childRef.current.closeMenu()} to='/photo'>Add Photo</Link>
-                    <Link className={classes.navItem} onClick={() => childRef.current.closeMenu()} to='/data' >Add Information</Link>
-                </>
-            }
-            <Link className={classes.navItem} onClick={() => childRef.current.closeMenu()} to='/gallery'>Image Gallery</Link>
-            <Link className={classes.navItem} onClick={() => childRef.current.closeMenu()} to='/colors'>Color References</Link>
-            <Link className={classes.navItem} onClick={() => childRef.current.closeMenu()} to='/info'>About Me</Link>
-            {/* <Link className={classes.navItem} onClick={closeMenu} to='/test'>Test Page</Link> */}
-
-            <LoginButton className={[classes.navItem, classes.navLoginButton].join(' ')} loginState={props.loginState} setLoginState={props.setLoginState} setUserState={props.setUserState} />
+            {navItems(true)}
         </MobileNavMenu>
     </nav>
 }
