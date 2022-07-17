10efab14975d2bda5a0562752243f396a871b844
alexk9081 <alexk9081@gmail.com>
2022-07-17 16:05:32 -0400

    Gave notifications background color prop

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,22 +1,23 @@
 import { useEffect, useState } from "react";
 import classes from "./Notification.module.css";
 
-function Notification({text}) {
+function Notification({ text, color }) {
     const [isActive, setIsActive] = useState(true);
 
     useEffect(() => {
         setTimeout(() => setIsActive(false), 1500);
     }, [])
 
-    return ( isActive &&
-        <div className={classes.notification} onClick={() => setIsActive(false)}>
+    return (isActive &&
+        <div className={classes.notification} style={{backgroundColor: color}} onClick={() => setIsActive(false)}>
             {text}
         </div>
     )
 }
 
 Notification.defaultProps = {
-    text: "This is a notification"
+    text: "This is a notification",
+    color: "#800"
 }
 
 export default Notification;
