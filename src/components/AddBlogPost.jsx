3299707a6ffa4661dc2cd162d155f21e4e175201
alexk9081 <alexk9081@gmail.com>
2022-07-28 23:59:34 -0400

    Added input fields to add blog post

--- src/components/AddBlogPost.jsx
+++ src/components/AddBlogPost.jsx
@@ -1,8 +1,15 @@
+import classes from "./AddBlogPost.module.css";
 
-function AddBlogPost() {
+function AddBlogPost(props) {
+    const submitPost = () => {}
+    const setTitle = () => {}
+    const setBody = () => {}
+
     return (
         <div>
-            ADD BLOG POST HERE
+            <input className={classes.titleInput} onChange={(newTitleInput) => { setTitle(newTitleInput.target.value) }} type="text" placeholder="Title" />
+            <textarea className={classes.bodyInput} onChange={(newBodyInput) => { setBody(newBodyInput.target.value) }} type="text" placeholder="Body" />
+            <input className={classes.submit} onClick={() => { submitPost() }} type="submit" />
         </div>
     )
 }
